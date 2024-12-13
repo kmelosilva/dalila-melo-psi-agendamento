6a3235e479055a0103c9fa1e0f1042fd13e09387
Kayque Melo Silva <kayque@Kayques-MacBook-Pro.local>
2024-12-12 23:10:05 -0300

    V1 API agendamentos Dalila-PSI (desenvolvimento de CRUD inicial de pacientes, usuarios, serviços e agendamentos)

--- back-end/routes/api.php
+++ back-end/routes/api.php
@@ -1,19 +1,10 @@
 <?php
+use App\Http\Controllers\PacienteController;
+use App\Http\Controllers\ServicoController;
+use App\Http\Controllers\AgendamentoController;
+use App\Http\Controllers\UserController;
 
-use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Route;
-
-/*
-|--------------------------------------------------------------------------
-| API Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "api" middleware group. Make something great!
-|
-*/
-
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
-});
+Route::apiResource('pacientes', PacienteController::class);
+Route::apiResource('servicos', ServicoController::class);
+Route::apiResource('agendamentos', AgendamentoController::class);
+Route::apiResource('usuarios', UserController::class);
