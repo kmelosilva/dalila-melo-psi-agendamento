43488737d7c61af0d4a5272dff9ee90a76733231
Kayque Melo Silva <kayque@Kayques-MacBook-Pro.local>
2025-04-13 14:38:11 -0300

    Adiciona dependecias bootstrap, pagina de serviços e componente navbar

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,23 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Home from './pages/Home';
+import Agendamentos from './pages/Agendamentos';
+import Pacientes from './pages/Pacientes';
+import Servicos from './pages/Servicos';
+import Layout from './components/Layout';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Layout>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/pacientes" element={<Pacientes />} />
+          <Route path="/agendamentos" element={<Agendamentos />} />
+          <Route path="/servicos" element={<Servicos />} />
+          <Route path="*" element={<h1>Página não encontrada</h1>} />
+        </Routes>
+      </Layout>
+    </Router>
   );
 }
 
